df8d8bc42353368b26fa8dc7f25b76353e3c5567
Roseokpe <roseokpe@gmail.com>
2022-09-24 10:03:30 -0700

    add class

--- list.rb
+++ list.rb
@@ -1,12 +1,7 @@
-# require_relative 'myenumerable'
+require_relative 'myenumerable'
 
-# class List
-#   include MyEnumerable
-#   def initialize(*args)
-#     @lists = args
-#   end
-
-#   def each
-#     @lists.each { |list| yield list if block_given? }
-#   end
-# end
+class List
+  include MyEnumerable
+  def initialize(*args)
+    @lists = args
+  end
