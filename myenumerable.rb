ebad3255472e605563c10a4f4fb8debbc4d4cfee
Roseokpe <roseokpe@gmail.com>
2022-09-24 10:06:13 -0700

    add myenumerable

--- myenumerable.rb
+++ myenumerable.rb
@@ -1,23 +1,23 @@
-# module MyEnumerable
-#     def all?(&block)
-#       each do |element|
-#         return false if block.call(element) == false
-#       end
-#       true
-#     end
+module MyEnumerable
+    def all?(&block)
+      each do |element|
+        return false if block.call(element) == false
+      end
+      true
+    end
   
-#     def any?(&block)
-#       each do |element|
-#         return true if block.call(element) == true
-#       end
-#       false
-#     end
+    def any?(&block)
+      each do |element|
+        return true if block.call(element) == true
+      end
+      false
+    end
   
-#     def filter(&block)
-#       arr_list = []
-#       each do |element|
-#         arr_list << element if block.call(element) == true
-#       end
-#       arr_list
-#     end
-#   end
+    def filter(&block)
+      arr_list = []
+      each do |element|
+        arr_list << element if block.call(element) == true
+      end
+      arr_list
+    end
+  end
